21a0bfb62d7e511594e9184762961ae31d2979f3
Yese <yese3982@gmail.com>
2022-03-01 17:23:19 -0500

    Learning python

---
+++ main.py
@@ -0,0 +1,20 @@
+# print("Hello World")
+
+# declaracion de variable, podemos colocar un nombre y le ponemos un igual y cuando queramos imprimir podemos llamar la variable. se pueden reutilizar.
+# mivariable = 3
+# print(mivariable)
+# print(mivariable)
+# mivariable = 9
+# print(mivariable)
+
+
+#la variabe se almacena en una posicion de memoria!
+x=10
+y=10
+z= x+y
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
